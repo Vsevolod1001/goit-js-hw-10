55ac9c7e47544719bfc27b8415007b1238ae0aca
Vsevolod1001 <sevant.101@gmail.com>
2021-11-18 23:34:53 +0200

    10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
+
+ const countries = function (markup) {
+    document.querySelector('.country-list').innerHTML = markup;
+  };
+  
+const oneCountry = function (markup) {
+    document.querySelector('.country-info').innerHTML = markup;
+  };
 
 const DEBOUNCE_DELAY = 300;
+
+const searchInput = document.querySelector('#search-box');
+
+searchInput.addEventListener('input', debounce(showCountries, DEBOUNCE_DELAY));
+
+function showCountries() {
+  if (searchInput.value.trim() === '') {
+    countries(' ');
+    oneCountry(' ');
+    return;
+  }
+  fetchCountries(searchInput.value.trim());
+}
+
+
+
